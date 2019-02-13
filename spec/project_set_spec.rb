76a7e363e148f126ebe05ee928f839b47a1c64ec
Sam Ehlers <groveriffic@gmail.com>
2019-02-13 08:47:08 -0600

    Spec to show a day is only ever counted once

--- spec/project_set_spec.rb
+++ spec/project_set_spec.rb
@@ -69,7 +69,12 @@ RSpec.describe ProjectSet do
   end
 
   context "#reimbursement_total" do
-    it "Any given day is only ever counted once, even if two projects are on the same day."
+    it "Any given day is only ever counted once, even if two projects are on the same day." do
+      project_a = Project.new(Date.new(2019, 2, 12), Date.new(2019, 2, 12), :low)
+      project_b = Project.new(Date.new(2019, 2, 12), Date.new(2019, 2, 12), :low)
+      project_set = ProjectSet.new([project_a, project_b])
+      expect(project_set.reimbursement_total).to eq 75
+    end
   end
 
   context "#city_cost" do
